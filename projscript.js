6838f6fac5561a7aefdf194af8dfcec1cfdd298b
stokesgarrett <89467122+stokesgarrett@users.noreply.github.com>
2023-06-10 00:54:07 -0600

    final commit

--- projscript.js
+++ projscript.js
@@ -1,16 +1,16 @@
 function redirectToCICIDS() {
-    // Redirect to another HTML page
+    // Redirect to CICIDS
     window.location.href = 'AnalyzationOfCICIDS2017.html';
   }
   function redirectToAWS() {
-    // Redirect to another HTML page
+    // Redirect to AWS
     window.location.href = 'AWSWebAppHosting.html';
   }
   function redirectToArista() {
-    // Redirect to another HTML page
+    // Redirect to Arista
     window.location.href = 'AristaNetworks.html';
   }
   function redirectToVuln() {
-    // Redirect to another HTML page
+    // Redirect to Vuln
     window.location.href = 'VulnerabilityManagement101.html';
   }
